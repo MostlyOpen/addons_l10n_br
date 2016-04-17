b35f8fcbdd3c8794ca18a0eb9fd7bfe6b90b8e2f
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-04-17 13:11:58 -0300

    Added module "l10n_br_clv_orz_medicament".

---
+++ l10n_br_clv_orz_medicament/orz_list/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# For copyright and license notices, see __openerp__.py file in root directory
+#
+###############################################################################
+
+import clv_orz_medicament_list
+import clv_orz_medicament_list_item
