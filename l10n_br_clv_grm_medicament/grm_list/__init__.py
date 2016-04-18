aaebe807ea2d7465ac722b669529876703913b02
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-04-18 16:37:29 -0300

    Added module "l10n_br_clv_grm_medicament".

---
+++ l10n_br_clv_grm_medicament/grm_list/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# For copyright and license notices, see __openerp__.py file in root directory
+#
+###############################################################################
+
+import clv_grm_medicament_list
+import clv_grm_medicament_list_item
---
+++ l10n_br_clv_grm_medicament/grm_list/clv_grm_medicament_list.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# For copyright and license notices, see __openerp__.py file in root directory
+#
+###############################################################################
+
+from openerp import models, fields
+
+
+class GRMMedicamentList(models.Model):
+    _name = 'clv_grm_medicament.list'
+
+    name = fields.Char('GRM List', required=True, size=64)
+    code = fields.Char('GRM List Code', size=128, required=False)
+    description = fields.Char(string='Description', size=256)
+    notes = fields.Text(string='Notes')
+    active = fields.Boolean('Active',
+                            help='The active field allows you to hide the list without removing it.',
+                            default=1)
+
+    _sql_constraints = [
+        ('uniq_list_name', 'unique(name)', "Error! The List Name must be unique!"),
+        ('uniq_list_code', 'unique(code)', "Error! The List Code must be unique!"),
+        ]
